74e75495f7680e6dac263fab421e0a85d124cf81
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-10-31 11:54:52 -0700

    new routes folder

--- routes/apiRoutes/fweetRoutes/index.js
+++ routes/apiRoutes/fweetRoutes/index.js
@@ -1,23 +1,30 @@
 const router = require('express').Router();
 
-const { json } = require('express');
-const { findAllFweetsApi, deleteFweetByIdApi, insertFweetApi, findFweetByIdApi, findFweetsByLoggedInUserApi } = require('../../../controllers/fweetController');
+const {
+  findAllFweetsApi,
+  deleteFweetByIdApi,
+  insertFweetApi,
+  findFweetByIdApi,
+  findFweetsByLoggedInUserApi,
+} = require('../../../controllers/fweetController');
 
 const authMiddleware = require('../../../middlewares/authorizationMiddleware');
 
-// /api/fweets/userfweets
+router.use(authMiddleware);
+
+// /api/fweets/userFweets
 router.route('/userfweets')
-    .get(findFweetsByLoggedInUserApi);
+  .get(findFweetsByLoggedInUserApi)
 
 // /api/fweets
 router.route('/')
-    .get(findAllFweetsApi);
-    .post(insertFweetApi);
+  .get(findAllFweetsApi)
+  .post(insertFweetApi);
 
 
-// /api/fweets/:fweetId
+//  /api/fweets/:fweetId
 router.route('/:fweetId')
-    .get(findFweetByIdApi)
-    .delete(deleteFweetByIdApi);
+  .get(findFweetByIdApi)
+  .delete(deleteFweetByIdApi);
 
 module.exports = router;
