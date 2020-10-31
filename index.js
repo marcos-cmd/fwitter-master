47f1926ba55df4ea0013e02128d1d71b1c3ee10b
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-10-31 11:46:46 -0700

    fixed index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 require('./services/passport');
 
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -16,8 +16,6 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-
-
 
 app.listen(PORT, () => {
   console.log('Server started listening on PORT http://localhost:3001');
